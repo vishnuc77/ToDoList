0840a4a92078566bf6025507ad4170efc7961f92
Ubuntu20 <vishnuc77@gmail.com>
2021-08-01 12:39:18 +0530

    adding all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+    const [tasks, setTasks] = useState ([
+      {
+          id: 1,
+          text: 'Buy',
+          day: 'Feb 5th',
+          reminder: true,
+      },
+      {
+          id: 2,
+          text: 'Sell',
+          day: 'Feb 12th',
+          reminder: true,
+      },
+      {
+          id: 3,
+          text: 'Food',
+          day: 'Feb 25th',
+          reminder: false,
+      },
+  ])
+
+  //Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id != id))
+  }
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id ===id 
+    ? { ...task, reminder: !task.reminder} : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+        ): (
+          'No tasks'
+        )}
     </div>
   );
 }
